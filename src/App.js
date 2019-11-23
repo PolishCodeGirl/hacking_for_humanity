d3d1d1d88e887a2b062d976eb6385bd1c6d953e6
Kuba Rodzen <kuba@rodzen.org>
2019-11-23 16:47:13 +0100

    Adding routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
 import RankingPage from './page/Ranking';
+import CityPage from "./page/City";
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App-header">
-      <RankingPage />
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App-header">
+              <Switch>
+                <Route path='/company-ranking' component={RankingPage} />
+                <Route path='/city-ranking' component={CityPage} />
+              </Switch>
+
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
