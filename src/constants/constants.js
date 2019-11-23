40ffa7c1d955f8ef5f75911f717735dbdd7a3bc2
Kuba Rodzen <kuba@rodzen.org>
2019-11-23 19:41:31 +0100

    Adding company avatars

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,6 +5,12 @@ import avatar4 from '../assets/avatar4.png';
 import avatar5 from '../assets/avatar5.png';
 import avatar6 from '../assets/avatar6.png';
 
+import company1 from '../assets/company1.png';
+import company2 from '../assets/company2.png';
+import company3 from '../assets/company3.png';
+import company4 from '../assets/company4.png';
+import company5 from '../assets/company5.png';
+
 export const peopleRanking = [
     { id: 1, name: 'Dimitri Ivanovic', points: '2500', percentage: '100%', avatar: avatar1, login: false },
     { id: 2, name: 'Anna Nowak', points: '2000', percentage: '80%', avatar: avatar6, login: false },
@@ -14,9 +20,9 @@ export const peopleRanking = [
 ];
 
 export const cityRanking = [
-    { id: 1, name: 'Skanska', points: '1792', percentage: '100%', avatar: avatar1 },
-    { id: 2, name: 'ACME Inc.', points: '1683', percentage: '62.5%', avatar: avatar2 },
-    { id: 3, name: 'LexCorp', points: '1525', percentage: '39%', avatar: avatar3 },
-    { id: 4, name: 'Industrial Science', points: '1410', percentage: '24%', avatar: avatar4 },
-    { id: 5, name: 'Ecological Company', points: '1364', percentage: '15%', avatar: avatar5 },
+    { id: 1, name: 'Skanska', points: '1792', percentage: '100%', avatar: company1 },
+    { id: 2, name: 'ACME', points: '1683', percentage: '62.5%', avatar: company2 },
+    { id: 3, name: 'High Building', points: '1525', percentage: '39%', avatar: company3 },
+    { id: 4, name: 'Smith & Smith', points: '1410', percentage: '24%', avatar: company4 },
+    { id: 5, name: '9 Lives Inc.', points: '1364', percentage: '15%', avatar: company5 },
 ]
