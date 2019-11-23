41e92914aab6ed1cc0878381f479b19195ba3280
Kuba Rodzen <kuba@rodzen.org>
2019-11-23 19:10:24 +0100

    Fixing avatar

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,10 +3,11 @@ import avatar2 from '../assets/avatar2.png';
 import avatar3 from '../assets/avatar3.png';
 import avatar4 from '../assets/avatar4.png';
 import avatar5 from '../assets/avatar5.png';
+import avatar6 from '../assets/avatar6.png';
 
 export const peopleRanking = [
     { id: 1, name: 'Dimitri Ivanovic', points: '2500', percentage: '100%', avatar: avatar1, login: false },
-    { id: 2, name: 'Anna Nowak', points: '2000', percentage: '80%', avatar: avatar2, login: false },
+    { id: 2, name: 'Anna Nowak', points: '2000', percentage: '80%', avatar: avatar6, login: false },
     { id: 3, name: 'Jan Kowalski', points: '1500', percentage: '60%', avatar: avatar3, login: true },
     { id: 4, name: 'John Smith', points: '1000', percentage: '40%', avatar: avatar4, login: false },
     { id: 5, name: 'John Snow', points: '500', percentage: '20%', avatar: avatar5, login: false },
